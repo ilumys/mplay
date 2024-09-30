f19850331c492479acd51c0261549a58b6aed22e
skyaadi <vcs@skyaa.dev>
2024-09-30 19:25:20 +1000

    use single sink

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use std::io::BufReader;
 use std::{fs::File, path::Path};
 
-use rodio::{Decoder, OutputStream, Sink};
-
 mod metadata;
 
 fn main() {
@@ -14,33 +12,36 @@ fn main() {
 }
 
 fn selecter(path: &Path) {
+    if let Some(tags) = metadata::get_tags(path) {
+        for tag in tags.iter() {
+            println!("{}: {}", tag.key, tag.value);
+        }
+    }
+    let (_stream, stream_handle) = rodio::OutputStream::try_default().expect("stream error");
+    let sink = rodio::Sink::try_new(&stream_handle).expect("sink error");
+
     if path.is_dir() {
         for file in path.read_dir().expect("dir error") {
             let file = file.expect("file extraction error").path();
             if file.is_dir() {
                 selecter(&file);
             } else if file.is_file() {
-                player(&file);
+                player(&file, &sink);
             }
         }
     } else if path.is_file() {
-        player(path)
+        player(&path, &sink);
     }
 }
 
-fn player(path: &Path) {
+fn player(path: &Path, sink: &rodio::Sink) {
     if let Some(tags) = metadata::get_tags(path) {
         for tag in tags.iter() {
             println!("{}: {}", tag.key, tag.value);
         }
     }
-    // should this be one sink per song or a persistent sink?
-    // requires study
-    let (_stream, stream_handle) = OutputStream::try_default().expect("stream error");
-    let sink = Sink::try_new(&stream_handle).expect("sink error");
-
     let data = BufReader::new(File::open(path).expect("bufreader error"));
-    let source = Decoder::new(data).expect("decode error");
+    let source = rodio::Decoder::new(data).expect("decode error");
     sink.append(source);
     sink.sleep_until_end();
 }
